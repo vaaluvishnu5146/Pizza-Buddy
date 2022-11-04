07ea4a76546bfbf68589dec6d2c80a869dccecee
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2022-11-04 16:00:14 +0530

    removed un touched slices for class

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,15 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import pizzaSlice from "./slice/pizza.slice";
-import cartSlice from "./slice/cart.slice";
-import todoSlice from "./slice/todo.slice";
 import taskSlice from "./slice/task.slice";
 
 // ROOT REDUCER
 export default configureStore({
   reducer: {
-    pizzaReducer: pizzaSlice,
-    cartReducer: cartSlice,
-    todoReducer: todoSlice,
     tasksReducer: taskSlice,
   },
 });
