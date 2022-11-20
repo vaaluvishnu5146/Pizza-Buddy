ed74b36f92cf30d6ab603b76115e88f37c3bdcc1
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2022-11-20 16:14:41 +0530

    B34 WE

--- src/App.js
+++ src/App.js
@@ -1,39 +1,52 @@
-import React, { useEffect, createRef } from "react";
+import React, { useEffect, createRef, useState } from "react";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Todo from "./Pages/dashboard/todo";
-import Form from "./Components/Form/Form";
+// import { Route, Routes } from "react-router-dom";
+// import Todo from "./Pages/dashboard/todo";
+// import Form from "./Components/Form/Form";
 import { addTodo } from "./Redux/slice/task.slice";
 import { useDispatch, useSelector } from "react-redux";
 
 function App() {
-  const taskInputRef = createRef(null);
-  const { tasks = [] } = useSelector((state) => state.tasksReducer);
   const dispatcher = useDispatch();
+  const { tasks = [] } = useSelector((state) => state.tasksReducer);
+  const [task, setTask] = useState("");
 
-  useEffect(() => {
-    console.log("Re-rendering");
-  }, []);
-
-  const handlesave = (e) => {
-    console.log(taskInputRef.current.value);
+  const saveTask = (e) => {
     if (e) {
-      dispatcher(addTodo(taskInputRef.current.value));
-      // setTask("");
+      dispatcher(addTodo(task));
+      setTask("");
+    }
+  };
+
+  const inputHandler = (e) => {
+    if (e) {
+      setTask(e.target.value);
     }
   };
 
   return (
     <div className="App">
-      <h1
-        style={{
-          margin: "0 0 20px 0",
-        }}
-        id="tasks-title"
-      >
-        My tasks ({tasks.length})
-      </h1>
-      <Form tasks={tasks} handlesave={handlesave} inputRef={taskInputRef} />
+      <h1>Tasks app</h1>
+      <div className="tasks-container">
+        <div className="tasks-form-container" id="tasks-form-container">
+          <input
+            className="text_input"
+            id="task-input"
+            placeholder="Enter task"
+            type="text"
+            onChange={inputHandler}
+            value={task}
+          />
+          <button className="task-cta" id="task-submit" onClick={saveTask}>
+            Save Task
+          </button>
+        </div>
+        <div id="tasks-list">
+          <ul>
+            {tasks && tasks.map((d, i) => <li key={`task-list-${i}`}>{d}</li>)}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
